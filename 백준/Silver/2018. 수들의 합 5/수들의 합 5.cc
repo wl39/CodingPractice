0c5ed1ef127d5f865dec19c13123750663411462
Lim <90633406+wl39@users.noreply.github.com>
2024-10-31 15:09:15 +0900

    [Silver V] Title: 수들의 합 5, Time: 32 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    int count = 1, sum = 1;
+
+    int start_index = 1, end_index = 1;
+
+    while (end_index != n)
+    {
+        if (sum > n)
+        {
+            sum -= start_index++;
+        }
+        else if (sum < n)
+        {
+            sum += ++end_index;
+        }
+        else
+        {
+            count++;
+            sum += ++end_index;
+        }
+    }
+
+    cout << count << '\n';
+}
