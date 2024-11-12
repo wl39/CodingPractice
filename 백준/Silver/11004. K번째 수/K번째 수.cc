2ba73f34ce5fd556c571baf908b4f69b5d5fec64
Lim <90633406+wl39@users.noreply.github.com>
2024-11-12 19:56:07 +0900

    [Silver V] Title: K번째 수, Time: 608 ms, Memory: 21552 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.cc
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -1,67 +1,110 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
-void quickSort(vector<int> &A, int S, int E, int K);
+
+typedef pair<int, int> node;
+int K;
 int partition(vector<int> &A, int S, int E);
-void swap(vector<int> &A, int i, int j);
+void q_sort(vector<int> &A, int S, int E);
 
-int main()
+void q_sort(vector<int> &A, int S, int E)
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    if (S < E)
+    {
+        int pivot = partition(A, S, E);
 
-    int N, K;
-    cin >> N >> K;
-    vector<int> A(N, 0);
-    for (int i = 0; i < N; i++) {
-        cin >> A[i];
+        if (K == pivot)
+        {
+            return;
+        }
+        else if (K < pivot)
+        {
+            q_sort(A, S, pivot - 1);
+        }
+        else
+        {
+            q_sort(A, pivot + 1, E);
+        }
     }
-    quickSort(A, 0, N-1, K-1);
-    cout << A[K - 1];
 }
 
- void quickSort(vector<int> &A, int S, int E, int K) {
-    int pivot = partition(A, S, E);
-    if (pivot == K)// K번째 수가 pivot이면 더이상 구할 필요 없음
-        return;
-    else if (K < pivot) // K가 pivot보다 작으면 왼쪽 그룹만 정렬 수행하기
-        quickSort(A, S, pivot - 1, K);
-    else // K가 pivot보다 크면 오른쪽 그룹만 정렬 수행하기
-        quickSort(A, pivot + 1, E, K);
-}
-int partition(vector<int> &A, int S, int E) {
-    if (S + 1 == E) {
-        if (A[S] > A[E])swap(A, S, E);
-        return E;
-    }
+int partition(vector<int> &A, int S, int E)
+{
     int M = (S + E) / 2;
-    swap(A, S, M); // 중앙값을 1번째 요소로 이동하기
-    int pivot = A[S];
-    int i = S + 1, j = E;
-    while (i <= j) {
-        while (j >= S + 1 && pivot < A[j]) {   //피벗보다 작은 수가 나올때까지 j--
-            j--;
-        }
-        while (i <= E && pivot > A[i] ) {  //피벗보다 큰 수가 나올 까지 i++
-            i++;
+    swap(A[E], A[M]);
+    int pivot = A[E];
+    int start = S;
+    int end = E - 1;
+
+    while (start <= end)
+    {
+        while (end >= S && A[end] > pivot)
+        {
+            end--;
         }
 
-        if (i < j) {
-            swap(A, i++, j--);  // 찾은 i와 j를 교환하기
+        while (start <= E - 1 && A[start] < pivot)
+        {
+            start++;
         }
-        else {
+
+        if (start < end)
+        {
+            swap(A[start++], A[end--]);
+        }
+        else
+        {
             break;
         }
     }
-    // i == j 피벗의 값을 양쪽으로 분리한 가운데에 오도록 설정하기
-    A[S] = A[j];
-    A[j] = pivot;
-    return j;
+
+    // cout << start << " " << end << "|" << S << " " << E << endl;
+
+    A[E] = A[end + 1];
+    A[end + 1] = pivot;
+    // A[end + 1] = A[]
+
+    // for (int i = 0; i < A.size(); i++)
+    // {
+    //     cout << A[i];
+    // }
+
+    // cout << endl;
+
+    return end + 1;
 }
-void swap(vector<int> &A, int i, int j) {
-    int temp = A[i];
-    A[i] = A[j];
-    A[j] = temp;
+
+int main()
+{
+    // how many times the value move from right to left;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    // 2 1 3 4 5
+    // 2 1 4 5 3
+    int N;
+
+    cin >> N >> K;
+    K--;
+
+    // int A[N];
+    vector<int> S(N, 0);
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> S[i];
+    }
+
+    q_sort(S, 0, N - 1);
+
+    // for (int i = 0; i < N; i++)
+    // {
+    //     cout << S[i];
+    // }
+
+    // cout << endl;
+
+    cout << S[K] << endl;
 }
